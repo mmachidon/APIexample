807fcbc6a81ee259b76b76d3f4ced166551f6711
mmachidon <m.machidon1@gmail.com>
2017-05-10 14:16:28 +0300

    commit

---
+++ APIexampleTest/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace APIexampleTest
+{
+    class Program
+    {
+        static string getToken()
+        {
+            var client = new RestClient("https://site06.qaw03.rxweb-dev.com/api/v2_3/security/login/AtfAdmin01/apikey");
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("host", "site06.qaw03.rxweb-dev.com");
+            request.AddHeader("x-apikey", "00000000-0000-0000-0000-000000000000");
+            request.AddHeader("x-lockmode", "Optimistic");
+            request.AddHeader("authorization", "Basic QXRmQWRtaW4wMTpUZXN0aW5nMTIz");
+            request.AddHeader("accept", "application/json");
+            request.AddHeader("content-type", "application/json");
+            request.AddHeader("user-agent", "Reed Exhibitions Nova SDK v3.7.0.0");
+            request.AddHeader("accept-encoding", "gzip,deflate");
+            request.AddParameter("application/json", "\"Testing123\"", ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            Console.WriteLine("Get token request {0}", response.StatusCode);
+            var obj = JObject.Parse(response.Content);
+            var token = (string)obj["Data"];
+            return token;
+        }
+        static string createUser(string token, User user)
+        {
+            string output = JsonConvert.SerializeObject(user);
+            Console.WriteLine("[" + output + "]");
+            var client1 = new RestClient("https://site06.qaw03.rxweb-dev.com/api/v2_3/users?disallowUpdate=False&sendEmail=True&upsertRegistration=True&contactableByObjectsInList=False&sendRegistrationEmail=True");
+            var request1 = new RestRequest(Method.POST);
+            request1.AddHeader("cache-control", "no-cache");
+            request1.AddHeader("content-type", "application/json");
+            request1.AddHeader("accept", "application/json");
+            request1.AddHeader("authorization", "Basic YXRmYWRtaW4wMTpUZXN0aW5nMTIz");
+            request1.AddHeader("host", "site06.qaw03.rxweb-dev.com");
+            request1.AddHeader("x-apikey", token);
+            request1.AddHeader("x-lockmode", "Optimistic");
+            request1.AddHeader("user-agent", "Reed Exhibitions Nova SDK v3.7.0.0");
+            request1.AddHeader("accept-encoding", "gzip,deflate");
+            request1.AddParameter("application/json", output, ParameterType.RequestBody);
+            IRestResponse response1 = client1.Execute(request1);
+            return response1.StatusCode.ToString();
+        }
+            static void Main(string[] args)
+        {
+           
+            User user = new User();
+            user.Username = "eg803t3@8323j2.reedtest";
+            user.__type = "User:#ReedExpo.Nova.Framework.Security";
+            user.IsNew = false;
+            user.IsLockedOut = false; 
+            user.ActivationId = "00000000-0000-0000-0000-000000000000";
+            user.UniqueId = "00000000-0000-0000-0000-000000000000";
+
+            user.CreatedDate = null;        
+            user.Email ="eg803t3@8323j2.reedtest";
+            MultilingualFirstName mFirstName= new MultilingualFirstName();
+            MultilingualLastName mLastName = new MultilingualLastName();
+            mFirstName.en="FN_08May1104s2362";
+            mFirstName.zh_HK = "FN_08May1104s2362";
+            mLastName.en = "LN_08May1104s2362";
+            mLastName.zh_HK = "LN_08May1104s2362";
+       
+            user.MultilingualFirstName = mFirstName;
+            user.MultilingualLastName = mLastName;
+            Extension extension = new Extension();
+            List<string> list = new List<string>();
+
+            extension.Attributes = list;
+
+            Address address = new Address();
+            AddressLine addresslLine = new AddressLine();
+            Country country= new Country();
+            Name name = new Name();
+            Id id= new Id();
+            id.Idvalue = "10";
+            id.ObjectTypeId = 111;
+         
+            name.ca = "Antàrtida";
+            name.de = "Antarktis";
+            country.CallingCode = "672";
+            country.DisplayName = "Antarctica";
+            country.Iso3Code = "ATA";
+            country.IsoCode = "AQ";
+            country.Id = id;
+            country.Name = name;
+            address.Country = country;
+            addresslLine.en = "Address Line 1";
+            address.IsNew = false;
+            address.AddressLine1 = addresslLine;
+            address.CountryId = 10;
+            address.Extension = extension;
+            user.Address = address;
+            user.Extension = extension;         
+            Console.WriteLine("Create User status Code {0}", createUser(getToken(),user));
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ APIexampleTest/User.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+namespace APIexampleTest
+{
+    public class Extension
+    {
+        public List <string>  Attributes { get; set; }
+        public object Categories { get; set; }
+        public object Contacts { get; set; }
+        public object Images { get; set; }
+        public object Packages { get; set; }
+        public object Url { get; set; }
+        public object Users { get; set; }
+
+    }
+     
+    public class AddressLine
+    {
+        public string en { get; set; }
+    }
+
+    public class Id
+    {
+        [JsonProperty("Id")]
+        public string Idvalue { get; set; }
+        [JsonProperty("Keys")]
+        public object Keys { get; set; }
+        [JsonProperty("ObjectTypeId")]
+        public int ObjectTypeId { get; set; }
+    }
+
+    public class Name
+    {
+        public string ar { get; set; }
+        public string ca { get; set; }
+        public string de { get; set; }
+        public string en { get; set; }
+        public string es { get; set; }
+        public string fr { get; set; }
+        public string id { get; set; }
+        public string it { get; set; }
+        public string ja { get; set; }
+        public string ko { get; set; }
+        public string pt { get; set; }
+        [JsonProperty("pt-BR")]
+        public string pt_BR { get; set; }
+        public string ru { get; set; }
+        public string sk { get; set; }
+        public string tr { get; set; }
+        public string vi { get; set; }
+        [JsonProperty("zh-CN")]
+        public string zh_CN { get; set; }
+        [JsonProperty("zh-HK")]
+        public string zh_HK { get; set; }
+    }
+
+    public class Country
+    {
+        public string CallingCode { get; set; }
+        public string DisplayName { get; set; }
+        public Id Id { get; set; }
+        public string Iso3Code { get; set; }
+        public string IsoCode { get; set; }
+        public Name Name { get; set; }
+    }
+
+    public class Address
+    {
+        public Extension Extension { get; set; }
+        public Id Id { get; set; }
+        public object __type { get; set; }
+        public bool IsNew { get; set; }
+        public object Tag { get; set; }
+        public AddressLine AddressLine1 { get; set; }
+        public AddressLine AddressLine2 { get; set; }
+        public AddressLine AddressLine3 { get; set; }
+        public object City { get; set; }
+        public Country Country { get; set; }
+        public int CountryId { get; set; }
+        public object PostCode { get; set; }
+        public object SubRegion { get; set; }
+    }
+
+    public class MultilingualFirstName
+    {
+        public string en { get; set; }
+        [JsonProperty("zh-HK")]
+        public string zh_HK { get; set; }
+    }
+
+    public class MultilingualLastName
+    {
+        public string en { get; set; }
+        [JsonProperty("zh-HK")]
+        public string zh_HK { get; set; }
+    }
+
+    public class User
+    {
+
+        public string __type { get; set; }
+        public Extension Extension { get; set; }
+        public Id Id { get; set; }
+        public bool IsNew { get; set; }
+        public object Tag { get; set; }
+        public object ActivationDate { get; set; }
+        public string ActivationId { get; set; }
+        public Address Address { get; set; }
+        public string CreatedDate { get; set; }
+        public object Culture { get; set; }
+        public string Email { get; set; }
+        public object Fax { get; set; }
+        public bool IsLockedOut { get; set; }
+        public object LockedOutDate { get; set; }
+        public object MiddleName { get; set; }
+        public object MobilePhoneNumber { get; set; }
+        public MultilingualFirstName MultilingualFirstName { get; set; }
+        public MultilingualLastName MultilingualLastName { get; set; }
+        public object Phone { get; set; }
+        public object Prefix { get; set; }
+        public string UniqueId { get; set; }
+        public string Username { get; set; }
+        public object WebsiteUrl { get; set; }
+    }
+}
+    
+
